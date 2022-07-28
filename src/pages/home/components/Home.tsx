796044c43b5979deccc9a37b3593e009098b21ab
R3laxation <memories@bk.ru>
2022-07-28 20:40:10 +0600

    added Home page

--- src/pages/home/components/Home.tsx
+++ src/pages/home/components/Home.tsx
@@ -1,5 +1,14 @@
 import React from 'react';
 
 import s from './Home.module.scss';
+import { ThisDay } from './thisDay/ThisDay';
+import { ThisDayInfo } from './thisDayInfo/ThisDayInfo';
 
-export const Home = () => <div className={s.home}>Home</div>;
+export const Home = () => (
+  <div className={s.home}>
+    <div className={s.wrapper}>
+      <ThisDay />
+      <ThisDayInfo />
+    </div>
+  </div>
+);
