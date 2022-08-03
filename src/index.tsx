6e8186fb96a3313787ae390692829c97f851faf7
R3laxation <memories@bk.ru>
2022-08-03 19:46:42 +0600

    add slices

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,21 @@
 import React from 'react';
 
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
 
 import App from './App';
 import './styles/index.scss';
 import { ThemeProvider } from './provider/ThemeProvider';
+import { store } from './store/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
-  <Router>
-    <React.StrictMode>
+  <Provider store={store}>
+    <Router>
       <ThemeProvider>
         <App />
       </ThemeProvider>
-    </React.StrictMode>
-  </Router>,
+    </Router>
+  </Provider>,
 );
