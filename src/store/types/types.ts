4f50cd2170663882a9a731a85a237d688ad1ffa6
R3laxation <memories@bk.ru>
2022-09-16 20:34:28 +0600

    changed Store Types

--- src/store/types/types.ts
+++ src/store/types/types.ts
@@ -1,5 +1,19 @@
 export type WeatherType = {
+  name: string;
   main: {
     temp: number;
+    feels_like: number;
+    pressure: number;
+    humidity: number;
+  };
+  wind: {
+    speed: number;
+  };
+};
+export type CityType = {
+  lat: number;
+  lon: number;
+  local_names: {
+    ru: string;
   };
 };
